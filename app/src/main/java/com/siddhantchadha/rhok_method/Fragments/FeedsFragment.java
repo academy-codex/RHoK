1804f15d55123275a5ab3670d72d37ad8abc0f68
Purujit Bansal <purujit.bansal9@gmail.com>
2017-07-16 05:28:32 +0530

    UI and Vid Call Left

--- app/src/main/java/com/siddhantchadha/rhok_method/Fragments/FeedsFragment.java
+++ app/src/main/java/com/siddhantchadha/rhok_method/Fragments/FeedsFragment.java
@@ -1,18 +1,21 @@
 package com.siddhantchadha.rhok_method.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.siddhantchadha.rhok_method.Activities.QueryTaken;
 import com.siddhantchadha.rhok_method.R;
 import com.siddhantchadha.rhok_method.models.Query;
 
@@ -41,6 +44,7 @@ public class FeedsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
 
@@ -52,16 +56,16 @@ public class FeedsFragment extends Fragment {
         feeds = createFeedsList(20);
 
         homeFeedAdapter = new FeedsAdapter(view.getContext(), feeds, type);
-        recyclerView.setAdapter(FeedsAdapter);
+        recyclerView.setAdapter(homeFeedAdapter);
 
         ctx = view.getContext();
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
-        horizontalDecoration = new DividerItemDecoration(recyclerView.getContext(),
-                DividerItemDecoration.VERTICAL);
-        horizontalDivider = ContextCompat.getDrawable(view.getContext(), R.drawable.horizontal_divider);
-        horizontalDecoration.setDrawable(horizontalDivider);
-        recyclerView.addItemDecoration(horizontalDecoration);
+//        horizontalDecoration = new DividerItemDecoration(recyclerView.getContext(),
+//                DividerItemDecoration.VERTICAL);
+//        horizontalDivider = ContextCompat.getDrawable(view.getContext(), R.drawable.horizontal_divider);
+//        horizontalDecoration.setDrawable(horizontalDivider);
+//        recyclerView.addItemDecoration(horizontalDecoration);
 
     }
 
@@ -78,4 +82,64 @@ public class FeedsFragment extends Fragment {
            return queryArrayList;
        }
 
+    private  class FeedsAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        ArrayList<Query> list;
+
+
+        public FeedsAdapter(Context context, ArrayList<Query> feeds, int type) {
+            this.list=feeds;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View itemview= LayoutInflater.from(getActivity()).inflate(R.layout.feeds_holder, parent, false);
+
+
+            return new MyViewHolder(itemview);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+            Query q= list.get(position);
+            holder.queryTv.setText(q.getQuery());
+            holder.takeQuery.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent= new Intent(getActivity(), QueryTaken.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+
+
+
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView queryTv;
+
+        Button takeQuery;
+
+
+        public MyViewHolder(View itemView) {
+
+
+
+            super(itemView);
+
+
+            queryTv= (TextView) itemView.findViewById(R.id.query);
+            takeQuery= (Button) itemView.findViewById(R.id.takeUp);
+        }
+    }
 }
